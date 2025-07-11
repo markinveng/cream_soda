bc6a7a6ea35b419c1ec34f0ab4bceeaa55c2a313
markinveng <markinveng.biz@gmail.com>
2025-07-12 01:59:28 +0900

    修正

--- src/js/loading.js
+++ src/js/loading.js
@@ -5,25 +5,24 @@ import gsap from "gsap";
  * @param {Function} onLoadComplete - ロード完了時のコールバック関数
  * @returns {Function} - 読み込み完了を通知する関数
  */
+
 export function setupLoading(onLoadComplete) {
   const loadingScreen = document.getElementById("loading-screen");
   let loadedAssets = 0;
   const totalAssets = 1;
-
-  return assetLoaded;
-}
-
-function assetLoaded() {
-  loadedAssets++;
-  if (loadedAssets === totalAssets) {
-    gsap.to(loadingScreen, {
-      opacity: 0,
-      duration: 1,
-      ease: "power1.inOut",
-      onComplete: () => {
-        loadingScreen.style.display = "none"; // フェードアウト完了後に非表示
-        if (onLoadComplete) onLoadComplete();
-      },
-    });
+  function assetLoaded() {
+    loadedAssets++;
+    if (loadedAssets === totalAssets) {
+      gsap.to(loadingScreen, {
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        onComplete: () => {
+          loadingScreen.style.display = "none"; // フェードアウト完了後に非表示
+          if (onLoadComplete) onLoadComplete();
+        },
+      });
+    }
   }
+  return assetLoaded;
 }
